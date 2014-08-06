0cb08e34af6fce1c356b6676a61fc18ff8e6df54
Eric Willigers <ericwilligers@chromium.org>
2014-08-06 13:06:31 +1000

    Initial support for repeatDur, min, max
    We now support simple scenarios where repeatDur, min, max are used.

--- test/testcases/circle-check.js
+++ test/testcases/circle-check.js
@@ -8,6 +8,8 @@ timing_test(function() {
   at(1000, 'cy', 275, polyfillCircle, nativeCircle);
   at(2000, 'r', 150, polyfillCircle, nativeCircle);
   at(3000, 'cx', 225, polyfillCircle, nativeCircle);
+  at(3750, 'cy', 206.25, polyfillCircle, nativeCircle);
+  at(3750, 'cx', 212.5, polyfillCircle, nativeCircle);
   at(4000, 'cy', 200, polyfillCircle, nativeCircle);
   at(5000, 'r', 100, polyfillCircle, nativeCircle);
 
