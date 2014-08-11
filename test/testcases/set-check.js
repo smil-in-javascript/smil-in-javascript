dd924b72e66809543df6166a747f5f285fb5486a
Eric Willigers <ericwilligers@chromium.org>
2014-08-11 15:28:56 +1000

    Animate SVG feComposite properties
    Add Web Animations Polyfill support for animation of the following SVG properties:-
    - k1
    - k2
    - k3
    - k4
    - refX
    - refY
    - rx
    - ry
    - scale
    - startOffset

--- test/testcases/set-check.js
+++ test/testcases/set-check.js
@@ -5,5 +5,7 @@ timing_test(function() {
   var nativeRect = document.getElementById('nativeRect');
 
   at(0, 'width', 200, polyfillRect, nativeRect);
+  at(1000, 'rx', 20, polyfillRect, nativeRect);
+  at(2000, 'ry', 10, polyfillRect, nativeRect);
   at(10000, 'width', 200, polyfillRect, nativeRect);
 }, 'set width');
