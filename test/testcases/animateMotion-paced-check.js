11fe3c045ab23116be883ac6252a062454766837
Eric Willigers <ericwilligers@chromium.org>
2014-08-25 21:29:38 +1000

    animateMotion calcMode defaults to 'paced' timing

--- test/testcases/animateMotion-paced-check.js
+++ test/testcases/animateMotion-paced-check.js
@@ -4,13 +4,16 @@ timing_test(function() {
   var polyfillRect = document.getElementById('polyfillRect');
   var nativeRect = document.getElementById('nativeRect');
 
+  var polyfillRect2 = document.getElementById('polyfillRect2');
+  var nativeRect2 = document.getElementById('nativeRect2');
+
   at(0, 'transform',
       ['translate(0, 0) rotate(0)', undefined],
       polyfillRect, nativeRect);
 
   at(15000, 'transform',
       ['translate(15, 0) rotate(0)', undefined],
-      polyfillRect, nativeRect);
+      polyfillRect2, nativeRect2);
 
   at(30000, 'transform',
       ['translate(30, 0) rotate(0)', undefined],
@@ -18,7 +21,7 @@ timing_test(function() {
 
   at(50000, 'transform',
       ['translate(30, 20) rotate(0)', undefined],
-      polyfillRect, nativeRect);
+      polyfillRect2, nativeRect2);
 
   at(70000, 'transform',
       ['translate(30, 40) rotate(0)', undefined],
@@ -26,7 +29,7 @@ timing_test(function() {
 
   at(103000, 'transform',
       ['translate(63, 40) rotate(0)', undefined],
-      polyfillRect, nativeRect);
+      polyfillRect2, nativeRect2);
 
   at(136000, 'transform',
       ['translate(96, 40) rotate(0)', undefined],
